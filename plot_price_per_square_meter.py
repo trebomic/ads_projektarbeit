5a49ace774f3273a2cb7fcca60501f04ba7cb350
trebomic <58138261+trebomic@users.noreply.github.com>
2023-05-06 16:12:15 +0200

    added plots with tensorflow and keras

--- plot_price_per_square_meter.py
+++ plot_price_per_square_meter.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
 
 # Laden des Excel-Files in ein Pandas DataFrame
 df = pd.read_excel('output_for_plot.xlsx')
@@ -17,8 +19,40 @@ df['flat_price_chf'] = df['flat_price_chf'].astype(float)
 # Berechnen des Preises pro Quadratmeter
 df['price_per_sqm'] = df['flat_price_chf'] / df['flat_size']
 
-# Erstellen des Scatterplots für die Preis pro Quadratmeter
-plt.scatter(df['flat_size'], df['price_per_sqm'])
-plt.xlabel('Grösse der Wohnung (in m²)')
-plt.ylabel('Preis pro Quadratmeter (in CHF/m²)')
+# Aufteilen der Daten in Trainings- und Testdaten
+train_dataset = df.sample(frac=0.8, random_state=0)
+test_dataset = df.drop(train_dataset.index)
+
+# Definieren der Eingabe-Features (Wohnungsgröße)
+train_features = train_dataset[['flat_size']]
+test_features = test_dataset[['flat_size']]
+
+# Definieren der Ausgabe-Labels (Preis pro Quadratmeter)
+train_labels = train_dataset[['price_per_sqm']]
+test_labels = test_dataset[['price_per_sqm']]
+
+# Definition des Modells
+model = keras.Sequential([
+    keras.layers.Dense(1, input_shape=[1])
+])
+
+# Kompilieren des Modells
+model.compile(loss='mean_squared_error',
+              optimizer=tf.keras.optimizers.Adam(0.1))
+
+# Trainieren des Modells
+history = model.fit(train_features, train_labels, epochs=100)
+
+# Vorhersagen treffen
+train_predictions = model.predict(train_features).flatten()
+test_predictions = model.predict(test_features).flatten()
+
+# Erstellen des Scatterplots für Wohnungsgröße und Preis pro Quadratmeter
+plt.scatter(train_features, train_labels, label='Daten')
+plt.scatter(train_features, train_predictions, color='green', label='Vorhersage')
+plt.scatter(test_features, test_labels, color='red', label='Test-Daten')
+plt.scatter(test_features, test_predictions, color='yellow', label='Vorhersage Test-Daten')
+plt.xlabel('Wohnungsgrösse')
+plt.ylabel('Preis pro Quadratmeter')
+plt.legend()
 plt.show()
