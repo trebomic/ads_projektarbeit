d686b41a5eda34849dc1cdc759e7c466252965e8
trebomic <58138261+trebomic@users.noreply.github.com>
2023-05-06 16:23:32 +0200

    added code with keras

--- plot_flat_size_and_price.py
+++ plot_flat_size_and_price.py
@@ -1,5 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from matplotlib import pyplot as plt
 
 # Laden des Excel-Files in ein Pandas DataFrame
 df = pd.read_excel('output_for_plot.xlsx')
@@ -14,8 +16,28 @@ df = df[df['number_of_rooms'] != 'None']
 df['flat_size'] = df['flat_size'].astype(float)
 df['flat_price_chf'] = df['flat_price_chf'].astype(float)
 
-# Plotten der Daten
+# Aufteilen der Daten in Trainings- und Testdaten
+train_data = df.sample(frac=0.8, random_state=0)
+test_data = df.drop(train_data.index)
+
+# Definition des Modells
+model = keras.Sequential([
+    keras.layers.Dense(units=1, input_shape=[1])
+])
+
+# Kompilieren des Modells
+model.compile(optimizer=tf.optimizers.Adam(1), loss='mean_squared_error')
+
+# Training des Modells
+history = model.fit(train_data['flat_size'], train_data['flat_price_chf'], epochs=100)
+
+# Evaluieren des Modells
+test_loss = model.evaluate(test_data['flat_size'], test_data['flat_price_chf'])
+print('Test Loss:', test_loss)
+
+# Plotten der Ergebnisse
 plt.scatter(df['flat_size'], df['flat_price_chf'])
+plt.plot(df['flat_size'], model.predict(df['flat_size']), color='red')
 plt.xlabel('Wohnfläche')
 plt.ylabel('Preis')
 plt.title('Preis vs Wohnfläche')
--- plot_price_per_room.py
+++ plot_price_per_room.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 # Laden des Excel-Files in ein Pandas DataFrame
 df = pd.read_excel('output_for_plot.xlsx')
@@ -17,8 +18,44 @@ df['flat_price_chf'] = df['flat_price_chf'].astype(float)
 # Berechnen des Preises pro Zimmer
 df['price_per_room'] = df['flat_price_chf'] / df['number_of_rooms']
 
-# Erstellen des Scatterplots für die Preis pro Zimmer
-plt.scatter(df['number_of_rooms'], df['price_per_room'])
+# Aufteilung in Trainings- und Testdaten
+train_df = df.sample(frac=0.8, random_state=42)
+test_df = df.drop(train_df.index)
+
+# Extrahieren der Merkmale (Features) und Zielvariablen (Targets)
+train_features = train_df[['number_of_rooms']].values
+train_labels = train_df['price_per_room'].values
+test_features = test_df[['number_of_rooms']].values
+test_labels = test_df['price_per_room'].values
+
+# Normalisieren der Features
+train_mean = train_features.mean()
+train_std = train_features.std()
+train_features = (train_features - train_mean) / train_std
+test_features = (test_features - train_mean) / train_std
+
+# Definition des Modells
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation='relu', input_shape=(1,)),
+    tf.keras.layers.Dense(1)
+])
+
+# Kompilieren des Modells
+model.compile(optimizer='adam', loss='mse')
+
+# Training des Modells
+history = model.fit(train_features, train_labels, epochs=100, verbose=0, validation_split=0.2)
+
+# Vorhersagen für Trainings- und Testdaten
+train_predictions = model.predict(train_features)
+test_predictions = model.predict(test_features)
+
+# Erstellen des Scatterplots für die Daten und Vorhersagen
+plt.scatter(train_df['number_of_rooms'], train_df['price_per_room'], color='blue', label='Trainingsdaten')
+plt.scatter(test_df['number_of_rooms'], test_df['price_per_room'], color='red', label='Testdaten')
+plt.scatter(train_df['number_of_rooms'], train_predictions, color='green', label='Trainingsvorhersagen')
+plt.scatter(test_df['number_of_rooms'], test_predictions, color='yellow', label='Testvorhersagen')
 plt.xlabel('Anzahl Zimmer')
 plt.ylabel('Preis pro Zimmer (in CHF)')
+plt.legend()
 plt.show()
