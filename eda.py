1767f0ae730e9a937fd388b71815d829edbba21f
trebomic <58138261+trebomic@users.noreply.github.com>
2023-05-06 13:58:19 +0200

    deleted "none" in the excel file and added code to plot data

--- eda.py
+++ eda.py
@@ -1,1 +1,23 @@
-# mitr 
+import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Laden des Excel-Files in ein Pandas DataFrame
+df = pd.read_excel('20230421_output.xlsx')
+
+# Entfernen von Zeilen mit NaN-Werten in der Spalte "number_of_rooms"
+df = df.dropna(subset=['number_of_rooms'])
+
+# Entfernen von Zeilen mit 'None' in der Spalte "number_of_rooms"
+df = df[df['number_of_rooms'] != 'None']
+
+# Konvertieren der Spalten "flat_size" und "flat_price_chf" in Float-Datentypen
+df['flat_size'] = df['flat_size'].astype(float)
+df['flat_price_chf'] = df['flat_price_chf'].astype(float)
+
+# Plotten der Daten
+plt.scatter(df['flat_size'], df['flat_price_chf'])
+plt.xlabel('Wohnfläche')
+plt.ylabel('Preis')
+plt.title('Preis vs Wohnfläche')
+plt.show()
